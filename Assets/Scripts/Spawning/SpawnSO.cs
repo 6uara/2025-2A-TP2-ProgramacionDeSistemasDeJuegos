25a11e62dd9506be5bc3e7cf97d87175dfa44c09
6uara <juanguaragnini@gmail.com>
2025-07-16 20:15:29 -0300

    Revert "Ejercicio2 (Falta anim exitosa)"
    This reverts commit d948c9c511122955eb539b668dbbd5ded47db98a.

--- Assets/Scripts/Spawning/SpawnSO.cs
+++ Assets/Scripts/Spawning/SpawnSO.cs
@@ -3,27 +3,8 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "SpawnSO", menuName = "Scriptable Objects/SpawnSO")]
 public class SpawnSO : ScriptableObject
 {
-    [Header("Digital Guy")]
-    public Character digitalprefab;
-    public CharacterModel digitalcharacterModel;
-    public PlayerControllerModel digitalcontrollerModel;
-    public RuntimeAnimatorController digitalanimatorController;
-
-    [Header("Pink Man")]
-    public Character pinkprefab;
-    public CharacterModel pinkcharacterModel;
-    public PlayerControllerModel pinkcontrollerModel;
-    public RuntimeAnimatorController pinkanimatorController;
-
-    [Header("Ninja Frog")]
-    public Character frogprefab;
-    public CharacterModel frogcharacterModel;
-    public PlayerControllerModel frogcontrollerModel;
-    public RuntimeAnimatorController froganimatorController;
-
-    [Header("Masked Dude")] 
-    public Character maskedprefab;
-    public CharacterModel maskedcharacterModel;
-    public PlayerControllerModel maskedcontrollerModel;
-    public RuntimeAnimatorController maskedanimatorController;
+    public Character prefab;
+    public CharacterModel characterModel;
+    public PlayerControllerModel controllerModel;
+    public RuntimeAnimatorController animatorController;
 }
